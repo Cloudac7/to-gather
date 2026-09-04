aaf13b9850e4dba722af8091a6ff194cc3668753
Cloudac7 <812556867@qq.com>
2026-09-04 14:28:39 +0800

    chore: track shared font stacks

---
+++ src/lib/fonts.ts
@@ -0,0 +1,3 @@
+export const UI_FONT_STACK = "system-ui, -apple-system, 'Segoe UI', 'PingFang SC', 'Microsoft YaHei', sans-serif";
+
+export const DISPLAY_FONT_STACK = "'Noto Serif SC', 'Songti SC', 'STSong', 'SimSun', serif";
